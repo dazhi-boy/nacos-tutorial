5a020fc8d0bde39e9acc6adf76b68d9c254eafb4
dazhi-boy <986095195@qq.com>
2021-03-08 23:03:12 +0800

    nacos配置管理添加注解

--- registry/src/main/java/com/dazhi/config/NacosConfigBootstrapConfiguration.java
+++ registry/src/main/java/com/dazhi/config/NacosConfigBootstrapConfiguration.java
@@ -15,19 +15,19 @@ import org.springframework.context.annotation.Configuration;
 public class NacosConfigBootstrapConfiguration {
     public NacosConfigBootstrapConfiguration() {
     }
-
+    // 读取配置文件，主要是注册中心的地址
     @Bean
     @ConditionalOnMissingBean
     public NacosConfigProperties nacosConfigProperties() {
         return new NacosConfigProperties();
     }
-
+    //将NacosConfigProperties封装
     @Bean
     @ConditionalOnMissingBean
     public NacosConfigManager nacosConfigManager(NacosConfigProperties nacosConfigProperties) {
         return new NacosConfigManager(nacosConfigProperties);
     }
-
+    // 从nacos中获取配置，将配置加载
     @Bean
     public NacosPropertySourceLocator nacosPropertySourceLocator(NacosConfigManager nacosConfigManager) {
         return new NacosPropertySourceLocator(nacosConfigManager);
